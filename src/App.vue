443a8598b69811b978f57ff262c0c2f3aa138471
简素言也 <409914837@qq.com>
2021-06-03 18:10:45 +0800

    /

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,27 @@ export default {
     // 获取设备信息
     uni.getSystemInfo({
       success: function (res) {
-        console.log(res.model);
-        console.log(res.pixelRatio);
-        console.log(res.windowWidth);
-        console.log(res.windowHeight);
-        console.log(res.language);
-        console.log(res.version);
-        console.log(res.platform);
+        console.log(res);
       },
     });
 
     // 获取用户信息
     uni.getUserInfo();
+    wx.login({
+      success(res) {
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: "https://example.com/onLogin",
+            data: {
+              code: res.code,
+            },
+          });
+        } else {
+          console.log("登录失败！" + res.errMsg);
+        }
+      },
+    });
   },
   onShow: function () {
     console.log("App Show");
@@ -30,4 +39,8 @@ export default {
 <style lang="scss">
 /*每个页面公共css */
 @import "uview-ui/index.scss"; /* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
+page {
+  height: 100%;
+  background: #fff;
+}
 </style>
---
+++ src/pages/index/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <view>
+
+    <view @click="getUserInfo">点击获取用户信息</view>
+  </view>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    // 2021年4月28日后微信变更后获取用户信息的新接口为 getUserProfile,必须用户点击触发，且每次都会弹窗授权
+    getUserInfo() {
+      console.log("获取用户信息userinfo");
+      uni.getUserProfile({
+        desc: "Wexin", // 这个参数是必须的
+        success: (res) => {
+          console.log("用户信息", res);
+        },
+        fail: (err) => {
+          console.log("获取用户信息失败", err);
+        },
+      });
+    },
+  },
+};
+</script>
+<style  lang='scss' scoped >
+</style>
