ad611a5b767aba04a4a4602bc56a3f7898113b72
简素言也 <409914837@qq.com>
2022-04-25 18:13:31 +0800

    add pinia

--- vue3-vite2-router4/src/main.js
+++ vue3-vite2-router4/src/main.js
@@ -3,5 +3,12 @@ import {
 } from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import {
+  createPinia
+} from 'pinia'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(createPinia())
+
+app.mount('#app')
