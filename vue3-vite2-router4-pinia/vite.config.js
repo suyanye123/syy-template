e063159a8450594a90e0df80dadad9c4f5329b65
简素言也 <409914837@qq.com>
2022-08-03 17:17:39 +0800

    /

--- vue3-vite2-router4-pinia/vite.config.js
+++ vue3-vite2-router4-pinia/vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    // vite config
+    plugins: [vue()],
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        //define global scss variable
+        scss: {
+          additionalData: `@import '@/styles/variables.scss';`,
+        },
+      },
+    },
+    server: {
+      port: 8077,
+      open: true, // 自动打开
+      base: "./",
+      proxy: {
+        // "/app/api": {
+        //   target: "https://dev.ylzpay.com/api/follow-up/app/api",
+        //   changeOrigin: true, // 打开代理
+        //   rewrite: path => path.replace("/app/api", ""),
+        // },
+      },
+    },
+  };
+});
